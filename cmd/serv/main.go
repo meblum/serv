b48189ca13f8a3c831b72dbe0da77d23ebb32258
Meir Blumenfeld <33358938+meblum@users.noreply.github.com>
2024-07-30 18:47:58 -0400

    refactor!: cleanup main.go
    Updates uses functions introduced in go 1.22

--- cmd/serv/main.go
+++ cmd/serv/main.go
@@ -29,54 +29,84 @@ func logInvalidPath(dirPath string) {
 	}
 }
 
-func main() {
-	log.SetFlags(0)
-	port := 8080
-	dir := "."
-	noReload := false
+type config struct {
+	port   int
+	dir    string
+	reload bool
+}
+
+func getOptions() config {
+	defaultConfig := config{
+		port:   8080,
+		dir:    ".",
+		reload: true,
+	}
 
 	fs := flag.NewFlagSet(filepath.Base(os.Args[0]), flag.ExitOnError)
 	fs.Usage = func() {
 		fmt.Fprintf(fs.Output(), "Usage: %s [-options] [directory (default \".\")]\n", fs.Name())
 		fs.PrintDefaults()
 	}
-	fs.IntVar(&port, "port", port, "port to serve on")
-	fs.BoolVar(&noReload, "no-reload", noReload, "serve without reloading on file update")
+
+	port := fs.Int("port", defaultConfig.port, "port to serve on")
+	noReload := fs.Bool("no-reload", !defaultConfig.reload, "serve without reloading on file update")
 	fs.Parse(os.Args[1:])
 	if fs.NArg() > 1 {
 		log.Fatalf("invalid arg %q (options must not be defined after file argument)", fs.Arg(1))
 	}
-
-	arg := fs.Arg(0)
-	if arg != "" {
-		dir = arg
+	dir := fs.Arg(0)
+	if dir == "" {
+		dir = defaultConfig.dir
 	}
 	logInvalidPath(dir)
-	sseContext, cancelSSE := context.WithCancel(context.Background())
-	defer cancelSSE()
-	fServer := serv.FileServer(sseContext, os.DirFS(dir))
-	if noReload {
-		fServer = http.FileServer(http.FS(os.DirFS(dir)))
+	return config{
+		port:   *port,
+		reload: !*noReload,
+		dir:    dir,
 	}
-	http.Handle("/", fServer)
+}
 
-	srv := &http.Server{Addr: fmt.Sprintf(":%v", port)}
+func fileServer(config config) *http.Server {
+	port := fmt.Sprintf(":%v", config.port)
+	dirFS := os.DirFS(config.dir)
+
+	if !config.reload {
+		return &http.Server{
+			Addr:    port,
+			Handler: http.FileServerFS(dirFS),
+		}
+	}
+
+	sseContext, cancelSSE := context.WithCancel(context.Background())
+	srv := &http.Server{
+		Addr:    port,
+		Handler: serv.FileServer(sseContext, dirFS),
+	}
 	srv.RegisterOnShutdown(cancelSSE)
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, os.Interrupt)
+	return srv
+}
+
+func main() {
+	log.SetFlags(0)
+	conf := getOptions()
+	server := fileServer(conf)
+
+	interruptCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	go func() {
-		log.Printf("serving files in \"%v\" at http://localhost%v", dir, srv.Addr)
-		err := srv.ListenAndServe()
+		log.Printf("serving files in \"%v\" at http://localhost%v", conf.dir, server.Addr)
+		err := server.ListenAndServe()
 		if err != http.ErrServerClosed {
 			log.Println(err)
 			os.Exit(1)
 		}
 	}()
 
-	<-shutdown
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	<-interruptCtx.Done()
+	shutDownCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
+	if err := server.Shutdown(shutDownCtx); err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/meblum/serv
 
-go 1.21.0
+go 1.22.0
